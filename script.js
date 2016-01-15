d6257dfaf876c71c62042020ebc4b08f3e801c61
David <Buch@Davids-MacBook-Air-5.local>
2016-01-14 19:44:05 -0500

    Completed Assignment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-num1 = prompt("We will add two numbers together. \nPlease input your first number for addition.");
-num2 = prompt("Please input a second number to add to the first.");
-Number(num1);
-Number(num2);
+snum1 = prompt("We will add two numbers together. \nPlease input your first number for addition.");
+snum2 = prompt("Please input a second number to add to the first.");
+num1 = Number(snum1);
+num2 = Number(snum2);
 alert("The result of your calculation is: \n" + num1 + " + " + num2 + " = " + (num1 + num2));
