b420ae8b043cdb0ba243f4926798df53294e91ee
jmathen2 <jmathen2@mix.wvu.edu>
2016-01-19 20:47:39 -0500

    "Add" Assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+//Josh Matheny "Add" Assignment
+var snum1 = prompt("What is the first number that you would like to add?");
+var snum2 = prompt("What is the second number that you would like to add?");
+
+var num1 = Number(snum1);
+var num2 = Number(snum2);
+
+var sum = num1 + num2;
+alert("The sum of your numbers is: " + sum);
