d9854bd571ff7ca90b4cab9bf8fe07b6c934b218
acurtis28 <Curtain@Austins-MacBook-Pro.local>
2016-01-21 15:34:29 -0500

    Added numbers

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstNumber = prompt("Enter the first number");
+firstNumber = Number(firstNumber);
+
+
+var secondNumber = prompt("Enter the second number");
+secondNumber = Number(secondNumber);
+
+
+var sum = firstNumber +secondNumber;
+alert("Sum of the two number is \n "+sum);
