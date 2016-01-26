0cbfe1af2eb67e51ba5f32fed4a1c190da9f6d7e
stadelmanma <stadelmanma@gmail.com>
2016-01-26 16:14:27 -0500

    completed isNaN modification

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function add() {
     //
     // getting user input
     var num1 = +document.getElementById('num-1').value;
+    if (isNaN(num1)) { alert('Error not a number.'); return;}
     var num2 = +document.getElementById('num-2').value;
+    if (isNaN(num2)) { alert('Error not a number.'); return;}
     //
     // adding number
     var result = parseFloat(num1) + parseFloat(num2);
