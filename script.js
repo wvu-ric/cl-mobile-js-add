9b2dde0ddcdcc1defca63ea3ca63f22f18ed5ccd
Karri <khitt@k12.wv.us>
2016-06-29 22:48:49 -0400

    added NaN check

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ var a = Number(prompt("Give me a number."));
 //prompt for second number
 var b = Number(prompt("Give me another number."));
 
-//display the sum of the two numbers
-alert(a + b);
+//check if input is a number
+	if (isNaN(a) || isNaN(b)) {
+		alert("Must input numbers.");
+	} else {
+//display the sum
+		var result = a + b;
+		alert("The sum is " + result);
+}
+
+
